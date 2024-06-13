3de08dcc67c451f1153e5948095cc92bbebcb233
Andre Victor <andreviictorx@gmail.com>
2024-06-12 22:14:51 -0300

    Resolução completa do exercicio para treinar os conhecimentos do DOM

---
+++ index.js
@@ -0,0 +1,35 @@
+const turnOn = document.getElementById('turnOn')
+const turnOff = document.getElementById('turnOff')
+const lamp = document.getElementById('lamp')
+
+
+//nesse caso, é crucial chamar a função no if pra fazer a verificação
+function isLampBroken(){
+    return lamp.src.indexOf('quebrada') > -1 //aqui ele verifica se no ato há  o src de quebrada
+}
+
+function lampOn(){
+    if(!isLampBroken()){ //qui ele so vai ativar a lampada se ela não estiver quebrada
+    lamp.src = './img/ligada.jpg';
+    }
+}
+
+function lampOff(){
+   if(!isLampBroken()){
+    lamp.src='./img/desligada.jpg';
+   } 
+}
+
+
+function lampBroken(){
+    lamp.src ='./img/quebrada.jpg';
+}
+
+turnOff.addEventListener('click', lampOff)
+turnOn.addEventListener('click', lampOn)
+
+
+lamp.addEventListener('mousemove', lampOn)
+lamp.addEventListener('mouseleave', lampOff)
+lamp.addEventListener('dblclick', lampBroken)
+
